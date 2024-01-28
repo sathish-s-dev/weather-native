6a1d87035ef964c95dec64ea335f7146691d9c3a
Sathish S <s.sathishmechddesign@gmail.com>
2024-01-28 17:59:55 +0530

    chore: initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  plugins: [],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	presets: [require('nativewind/preset')],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#4B3EAE',
+				secondary: '#F8F8F8',
+			},
+		},
+	},
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	plugins: [],
 };
